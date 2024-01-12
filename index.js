1556174873fe931f59ad0955d6c9c271ef3f71a0
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2024-01-12 08:44:36 +0530

    root: repo init with some files

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+//a valid js code is also a valid ts code
+//will get an error if variable of same name is there in another file also
+let fName = 'chintu';
+let favInt = 83;
+let favFloat = 83.1;
+let favNeg = -83;
+console.log(favInt);
+console.log(favInt.toString());
+console.log(favFloat);
+console.log(favNeg);
---
+++ index.ts
@@ -0,0 +1,24 @@
+//a valid js code is also a valid ts code
+//tsconfig.js -> tsc --init
+//will get an error if variable of same name is there in another file also
+
+//hw: 
+//use NaN with number variable
+//substring
+//string comparision
+//string template
+
+let fName:string = 'chintu'
+let favInt:number = 83
+let favFloat:number = 83.1
+let favNeg:number = -83
+
+console.log(favInt);
+console.log(favInt.toString());
+console.log(favFloat);
+console.log(favNeg);
+
+
+
+
+
